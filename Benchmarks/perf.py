02d16607e4d458414942dd84defe1ee221b12a5c
gwhite3629 <grady.white@maine.edu>
2022-12-01 18:17:16 -0500

    Added Data

---
+++ Benchmarks/perf.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import math as m
+import numpy as np
+import re
+
+class Event(object):
+    def __init__(self, name):
+
+    def parse(self):
+
+class Test(object):
+    def __init__(self, name):
